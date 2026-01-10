713d4e4affb046c4be35e56b8f805c54e89d7835
chyong007 <chyong007@gmail.com>
2026-01-10 14:23:09 +0800

    Update NLP-assignment-01.py

--- NLP-assignment-01.py
+++ NLP-assignment-01.py
@@ -67,7 +67,7 @@ sent = input.split()
 lw_input=[wd.lower() for wd in sent]
 
 # Normalize fancy quotes
-text = re.sub(r"[“”]", '"', lw_input)
+#text = re.sub(r"[“”]", '"', lw_input)
 text = re.sub(r"[’]", "'", lw_input)
 
 # Tokenize with re.split()
@@ -297,6 +297,7 @@ with col4:
 #*****************************************************************************#   
 
 st.write("Streamlit Version:", st.__version__)
+
 
 
 
